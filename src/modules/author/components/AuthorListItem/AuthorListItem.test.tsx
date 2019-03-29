0a443943c9ec7a3efad4b6cdf1f83af4c664f476
starodubenko <r.starodubenko@tinkoff.ru>
2019-03-29 23:38:28 +0300

    removed useless tests for a while

--- src/modules/author/components/AuthorListItem/AuthorListItem.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {AuthorListItem} from './AuthorListItem';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AuthorListItem />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/modules/book/components/BookIEdittem/BookEditItem.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BookEditItem} from './BookEditItem';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<BookEditItem />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
